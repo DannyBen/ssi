8000b8dcb47fb2696ba2133f2b36518d8e213026
Danny Ben Shitrit <db@dannyben.com>
2026-03-07 19:00:18 +0200

    prefer curl in bootstrap template

--- src/commands/bootstrap.sh
+++ src/commands/bootstrap.sh
@@ -5,22 +5,23 @@ set -euo pipefail
 
 # === Setup SSI ===
 
+echo "Initializing installer..."
 tmpdir="$(mktemp -d)"
 trap 'rm -rf "$tmpdir"' EXIT
 cd "$tmpdir"
 export PATH="$tmpdir:$PATH"
 
-if command -v wget >/dev/null 2>&1; then
-  wget -nv -O ssi https://github.com/DannyBen/ssi/releases/download/v@VERSION@/ssi
-elif command -v curl >/dev/null 2>&1; then
+if command -v curl >/dev/null 2>&1; then
   curl -fSsL https://github.com/DannyBen/ssi/releases/download/v@VERSION@/ssi -o ssi
+elif command -v wget >/dev/null 2>&1; then
+  wget -nv -O ssi https://github.com/DannyBen/ssi/releases/download/v@VERSION@/ssi
 else
   echo "Error: please install wget or curl, then try again" >&2
   exit 1
 fi
 chmod +x ssi
 
-# === Install ===
+# === Run ===
 
 # Example installation steps:
 # ssi log info "Installing <tool>"
